02aae201cd36896a715a5dabf18d39fdc18624ac
o-clipe <luksnapo@gmail.com>
2024-01-19 21:28:37 -0300

    Add Q-learning snake game.

---
+++ q_learning/main.py
@@ -0,0 +1,110 @@
+from snake import Snake, inference, cancel_inference
+from random import randint
+import numpy as np
+import time
+import curses
+
+GRID = [11, 11]
+RAND_GRID = lambda: [randint(3, 11) * 2 - 1 for _ in range(2)]
+new_game = lambda: Snake(RAND_GRID(), 2)
+
+Q = np.zeros(shape=(4, 8, 2, 2, 2, 2, 4), dtype='float32')  # [direction (4), food (8), danger(2^4), actions (4)]
+
+
+def Q_idx(game: Snake):
+    head = game.position[0]
+    x_direction = head[0] - game.position[1][0]
+    y_direction = head[1] - game.position[1][1]
+    direction = 0
+
+    if x_direction < 0:
+        direction = 0
+    if x_direction > 0:
+        direction = 1
+    if y_direction < 0:
+        direction = 2
+    if y_direction > 0:
+        direction = 3
+
+    x_candy = game.candy[0] - head[0]
+    y_candy = game.candy[1] - head[1]
+    candy = -1
+
+    if x_candy < 0:
+        if y_candy < 0:
+            candy = 0
+        elif y_candy == 0:
+            candy = 7
+        elif y_candy > 0:
+            candy = 6
+    elif x_candy == 0:
+        if y_candy < 0:
+            candy = 1
+        elif y_candy > 0:
+            candy = 5
+    elif x_candy > 0:
+        if y_candy < 0:
+            candy = 2
+        if y_candy == 0:
+            candy = 3
+        if y_candy > 0:
+            candy = 4
+
+    danger = 0
+
+    def check_point(point):
+        for pixel in game.position:  # stumbles
+            if point[0] == pixel[0] and point[1] == pixel[1]:
+                return True
+        for pt, max in zip(point, game.grid):  # hits wall
+            if pt >= max or pt < 0:
+                return True
+        return False
+
+    nx = 1 if check_point((head[0] - 1, head[1])) else 0
+    px = 1 if check_point((head[0] + 1, head[1])) else 0
+    ny = 1 if check_point((head[0], head[1] - 1)) else 0
+    py = 1 if check_point((head[0], head[1] + 1)) else 0
+
+    for exp, do in zip(reversed(range(4)), (nx, px, ny, py)):
+        danger += 2 ** exp if do else 0
+
+    assert direction >= 0 and candy >= 0
+    return direction, candy, nx, px, ny, py
+
+
+def train():
+    iterations = 1_000_000
+    for i in range(iterations):
+        game = new_game()
+        for j in range(1000):
+            if not game.alive:
+                break
+            observations = Q_idx(game)
+            a = randint(0, i // (iterations // 10))
+            if a:
+                action = np.argmax(Q[observations])
+            else:
+                action = randint(0, 3)
+            currentQ = Q[observations + (action,)]
+            reward = game.move(action)
+            print(f"\r{i}/{iterations}", end='')
+            maxQ = np.max(Q[Q_idx(game)])
+            Q[observations + (action,)] = currentQ + 0.1 * (reward + 0.2 * maxQ - currentQ)
+
+
+def test():
+    game = new_game()
+    for i in range(10000):
+        time.sleep(0.1)
+        if not game.alive:
+            game = Snake(GRID, 2)
+        observations = Q_idx(game)
+        action = np.argmax(Q[observations])
+        inference(game, action, f"{Q[observations]}: {np.argmax(Q[observations])} with Q idx :{observations}")
+
+    cancel_inference()
+
+
+train()
+test()
---
+++ q_learning/snake.py
@@ -0,0 +1,154 @@
+import random
+import curses
+
+GRID = [11, 11]
+START_SIZE = 1
+ZERO = 0
+
+class Snake:
+    def __init__(self, grid, length):
+        self.grid = grid
+        self.dimensions = len(grid)
+        self.position = [[d // 2 for d in self.grid] for _ in range(length)]
+        self.place_candy()
+        self.alive = True
+        self.score = len(self.position) * 100
+
+
+    def place_candy(self):
+        self.candy = [random.randint(0, dimension - 1) for dimension in self.grid]
+        if self.candy in self.position:
+            self.place_candy()
+
+    def move(self, direction: int):
+        """Take as direction 2 => [0, 0, 1, 0] => -1 second dimension ... 1 => [0, 1, 0, 0] => +1 first dimension"""
+        sign = -1 if direction % self.dimensions == 0 else 1
+        dimension = direction // 2
+        new_pos = self.position[0].copy()
+        new_pos[dimension] += sign
+        return self.event(new_pos)
+
+    def event(self, new_point):
+        if new_point in self.position:  # stumbles
+            return self.die()
+        for pt, max in zip(new_point, self.grid):  # hits wall
+            if pt >= max or pt < 0:
+                return self.die()
+        if new_point == self.candy:
+            self.position.insert(0, new_point)  # eats candy
+            self.place_candy()
+            self.score += 100 - self.score % 100
+            return self.turn(100)
+        else:
+            self.position.insert(0, new_point)  # moves
+            self.position.pop()
+            self.score += 1 if self.score % 100 != 99 else 0
+            return 1
+
+    def turn(self, reward):
+        return reward
+
+    def die(self):
+        self.alive = False
+        return -100
+
+
+def visualize_game(game):
+    scrgrid = [["" for _ in range(game.grid[0] + 2)] for __ in range(game.grid[1] + 2)]
+    for i in range(game.grid[0] + 2):
+        for j in range(game.grid[1] + 2):
+            scrgrid[j][i] = "w" if (i == 0 or i == game.grid[0] + 1 or j == 0 or j == game.grid[1] + 1) else " "
+
+    for pixel in game.position:
+        scrgrid[pixel[1]+1][pixel[0]+1] = "\u25A0"
+
+    scrgrid[game.candy[1]+1][game.candy[0]+1] = "*"
+    return scrgrid
+
+def main(stdscr):
+    curses.noecho()
+    curses.cbreak()
+    stdscr.keypad(True)
+    game = Snake(GRID, START_SIZE)
+    while game.alive == True:
+
+        scrgrid = visualize_game(game)
+        for line in scrgrid:
+            stdscr.addstr(" ".join(line) + "\n")
+
+        move = stdscr.getch()
+        if move == curses.KEY_LEFT:
+            game.move(int(0))
+            stdscr.clear()
+        if move == curses.KEY_RIGHT:
+            game.move(int(1))
+            stdscr.clear()
+        if move == curses.KEY_UP:
+            game.move(int(2))
+            stdscr.clear()
+        if move == curses.KEY_DOWN:
+            game.move(int(3))
+            stdscr.clear()
+
+
+    curses.nocbreak()
+    stdscr.keypad(False)
+    curses.echo()
+    curses.endwin()
+    print("YOU DIED")
+
+
+class Inference:
+    _instance = None
+    def __new__(class_, *args, **kwargs):
+        if not isinstance(class_._instance, class_):
+            class_._instance = object.__new__(class_)
+        return class_._instance
+
+    def __init__(self):
+        self._stdscr = curses.initscr()
+        curses.noecho()
+        curses.cbreak()
+
+    def __call__(self, game: Snake, action: int, bottom_text: str):
+        self._stdscr.clear()
+        scrgrid = visualize_game(game)
+        for line in scrgrid:
+            self._stdscr.addstr(" ".join(line) + "\n")
+        self._stdscr.addstr(bottom_text)
+        reward = game.move(int(action))
+        self._stdscr.refresh()
+        return reward
+
+    def __del__(self):
+        curses.nocbreak()
+        curses.echo()
+        curses.endwin()
+
+
+def inference(game: Snake, action: int, bottom_text=None):
+    obj = Inference()
+    return obj(game, action, bottom_text if bottom_text is not None else "")
+
+
+def cancel_inference():
+    curses.nocbreak()
+    curses.echo()
+    curses.endwin()
+
+
+
+if __name__ == "__main__":
+    stdscr = curses.initscr()
+    curses.wrapper(main)
+
+    # import time
+    #
+    # game = Snake(GRID, 2)
+    # for i in range(1000):
+    #     time.sleep(0.03)
+    #     if game.alive == False:
+    #         game = Snake(GRID, 2)
+    #     inference(game, 1)
+
+
